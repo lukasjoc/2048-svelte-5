0c41d24d34fb146e9b20e460ef17ab7e034e73aa
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2024-10-27 15:45:29 +0100

    feat: random seed new card

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { toRightTransformed, toLeftTransformed } from "./game";
+
     function choose(...items: number[]) {
         const idx = Math.floor(Math.random() * items.length);
         return items[idx];
@@ -7,8 +9,8 @@
     function randN(N: number) {
         return Math.floor(Math.random() * N);
     }
-
     function seed() {
+        console.log("Seed");
         const board: { value: number | null }[][] = [];
         for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
             let row = [];
@@ -33,12 +35,45 @@
     const seeded = seed();
     const board = $state(seeded);
 
+    function chooseNextRandomField() {
+        let indices = board
+            .flat()
+            .reduce(
+                (x, curr, i) => (curr.value === null ? [...x, i] : x),
+                [] as number[],
+            );
+        let idx = choose(...indices);
+        let row = Math.floor(idx / board.length);
+        let col = Math.max(row, row - 3);
+        board[row][col] = { value: choose(2, 4) };
+        console.log(idx);
+    }
+
     function onkeyup(event: KeyboardEvent) {
         console.log(event);
         switch (event.key) {
             case "ArrowRight":
+                for (let i = 0; i < board.length; i++) {
+                    let row = board[i].map((x) => x.value ?? 0);
+                    let transformed = toRightTransformed(row);
+                    console.log("Index: ", i, row, transformed);
+                    let ret = transformed.map((x) => ({
+                        value: x == 0 ? null : x,
+                    }));
+                    board[i] = ret;
+                }
+                chooseNextRandomField();
                 break;
             case "ArrowLeft":
+                for (let i = 0; i < board.length; i++) {
+                    let row = board[i].map((x) => x.value ?? 0);
+                    let transformed = toLeftTransformed(row);
+                    console.log("Index: ", i, row, transformed);
+                    let ret = transformed.map((x) => ({
+                        value: x == 0 ? null : x,
+                    }));
+                    board[i] = ret;
+                }
                 break;
             case "ArrowDown":
                 break;
@@ -49,6 +84,7 @@
                 return;
         }
     }
+    $inspect(board);
 </script>
 
 <svelte:window {onkeyup} />
@@ -56,12 +92,12 @@
 <div class="board">
     {#each board as row}
         <div class="row">
-            {#each row as cell}
+            {#each row as cell, i}
                 <div
                     class="cell"
                     style={cell.value ? "background-color: white;" : ""}
                 >
-                    {cell.value}
+                    {i}{cell.value}
                 </div>
             {/each}
         </div>
@@ -75,15 +111,20 @@
     }
 
     .board {
-        display: flex;
+        /* display: flex; */
         width: fit-content;
         background-color: black;
+    }
+    .row {
+        display: flex;
+        flex-direction: row;
     }
 
     .cell {
         width: var(--cell-width);
         height: var(--cell-height);
         margin: 3px;
-        display: flex;
+        /* display: flex; */
+        color: red;
     }
 </style>
--- src/lib/game.ts
+++ src/lib/game.ts
@@ -14,52 +14,116 @@ function toRightShifted(row: number[]): number[] {
 }
 
 function toRightCombined(row: number[]): number[] {
-    let combined = row.slice();
-    for (let i = combined.length - 1; i >= 0; i--) {
+    let ret = row.slice();
+    for (let i = ret.length - 1; i >= 0; i--) {
         let rhs = i;
         let lhs = i - 1;
-        if (lhs === -1) break;
+        // if (lhs === -1) break;
         // if rhs === 0 and lhs is non zero combine to lhs if moving right
-        console.log(lhs, rhs, "<|>", combined[lhs], combined[rhs])
-        if (combined[rhs] === 0 && combined[lhs] === 0) continue;
-        let valuesMatch = combined[rhs] === combined[lhs];
-        let canShiftToRight = combined[rhs] === 0;
-        if (valuesMatch || canShiftToRight) {
-            console.log("\t transform:combine", rhs, lhs)
-            let res = combined[rhs] + combined[lhs];
-            combined.splice(lhs, 2); // remove the pair (only one combination in move)
-            combined.push(res); // push the result
-            combined.unshift(0); // pad the left side as we move to the right
+        // console.log(lhs, rhs, "<|>", ret[lhs], ret[rhs])
+        if (ret[rhs] === 0 && ret[lhs] === 0) continue;
+        if (ret[rhs] === ret[lhs]) {
+            // console.log("\t transform:combine", rhs, lhs)
+            let res = ret[rhs] + ret[lhs];
+            ret.splice(lhs, 2); // remove the pair (only one combination in move)
+            ret.push(res); // push the result
+            ret.unshift(0); // pad the left side as we move to the right
         }
-        console.log("Transform : ", combined);
+        // console.log("Transform : ", ret);
     }
-    return combined;
+    return ret;
 }
 
-function transformRight1(row: number[]): number[] {
+
+function toLeftShifted(row: number[]): number[] {
+    const zeros = [];
+    for (let i = 0; i < row.length; i++) {
+        if (row[i] !== 0) break;
+        zeros.unshift(i);
+    }
+    if (zeros.length === 0) {
+        return row.slice();
+    }
+    let cropped = row.slice();
+    cropped.splice(zeros[zeros.length - 1], zeros.length);
+    cropped.push(...zeros.map(_ => 0))
+    return cropped;
+}
+
+function toLeftCombined(row: number[]): number[] {
+    let ret = row.slice();
+    for (let i = 0; i < ret.length; i++) {
+        let rhs = i + 1;
+        let lhs = i;
+        if (rhs >= ret.length) break;
+        // if rhs === 0 and lhs is non zero combine to lhs if moving right
+        console.log(lhs, rhs, "<|>", ret[lhs], ret[rhs])
+        if (ret[rhs] === 0 && ret[lhs] === 0) continue;
+        if (ret[rhs] === ret[lhs]) {
+            console.log("\t transform:combine", lhs, rhs)
+            let res = ret[rhs] + ret[lhs];
+            ret.splice(lhs, 2); // remove the pair (only one combination in move)
+            ret.unshift(res); // push the result
+            ret.push(0); // pad the left side as we move to the right
+        }
+        console.log("Transform : ", ret);
+    }
+    return ret;
+}
+
+export function toRightTransformed(row: number[]): number[] {
     let shifted = toRightShifted(row);
-    let combined = toRightCombined(shifted);
-    return combined;
+    let ret = toRightCombined(shifted);
+    return ret;
 }
 
+export function toLeftTransformed(row: number[]): number[] {
+    let shifted = toLeftShifted(row);
+    console.log("shifted", shifted)
+    let ret = toLeftCombined(shifted);
+    return ret;
+}
 if (import.meta.vitest) {
     const { it, expect } = import.meta.vitest
-    let cases = [
-        [/*input*/[0, 2, 0], /*expected*/[0, 0, 2]],
-        [/*input*/[2, 4, 0], /*expected*/[0, 2, 4]],
-        [/*input*/[0, 0, 2], /*expected*/[0, 0, 2]],
-        [/*input*/[2, 0, 0], /*expected*/[0, 0, 2]],
-        [/*input*/[2, 2, 2], /*expected*/[0, 2, 4]],
-        [/*input*/[4, 4, 0], /*expected*/[0, 0, 8]],
-        [/*input*/[0, 4, 4], /*expected*/[0, 0, 8]],
-        [/*input*/[8, 4, 2], /*expected*/[8, 4, 2]],
-        [/*input*/[8, 8, 8], /*expected*/[0, 8, 16]],
-        [/*input*/[4, 4, 4], /*expected*/[0, 4, 8]],
-    ];
-
     it('can move right', () => {
+        let cases = [
+            [/*input*/[0, 2, 0], /*expected*/[0, 0, 2]],
+            [/*input*/[2, 4, 0], /*expected*/[0, 2, 4]],
+            [/*input*/[0, 0, 2], /*expected*/[0, 0, 2]],
+            [/*input*/[2, 0, 0], /*expected*/[0, 0, 2]],
+            [/*input*/[2, 2, 2], /*expected*/[0, 2, 4]],
+            [/*input*/[4, 4, 0], /*expected*/[0, 0, 8]],
+            [/*input*/[0, 4, 4], /*expected*/[0, 0, 8]],
+            [/*input*/[8, 4, 2], /*expected*/[8, 4, 2]],
+            [/*input*/[8, 8, 8], /*expected*/[0, 8, 16]],
+            [/*input*/[4, 4, 4], /*expected*/[0, 4, 8]],
+            [/*input*/[4, 0, 4], /*expected*/[0, 0, 8]], //NOK
+        ];
+
         for (const [input, expected] of cases) {
-            expect(transformRight1(input)).toStrictEqual(expected)
+            expect(toRightTransformed(input)).toStrictEqual(expected)
+        }
+    })
+
+    // TODO:
+    // need to start the shifting on the right most side where we want to move and
+    // shift pairwise when possible instead of group wise
+    it('can move left', () => {
+        let cases = [
+            [/*input*/[0, 2, 0], /*expected*/[2, 0, 0]],
+            [/*input*/[2, 4, 0], /*expected*/[2, 4, 0]],
+            [/*input*/[0, 0, 2], /*expected*/[2, 0, 0]],
+            [/*input*/[2, 0, 0], /*expected*/[2, 0, 0]],
+            [/*input*/[2, 2, 2], /*expected*/[4, 2, 0]],
+            [/*input*/[4, 4, 0], /*expected*/[8, 0, 0]],
+            [/*input*/[0, 4, 4], /*expected*/[8, 0, 0]],
+            [/*input*/[8, 4, 2], /*expected*/[8, 4, 2]],
+            [/*input*/[8, 8, 8], /*expected*/[16, 8, 0]],
+            [/*input*/[4, 4, 4], /*expected*/[8, 4, 0]],
+            // [/*input*/[4, 0, 4], /*expected*/[8, 0, 0]], //NOK
+        ];
+        for (const [input, expected] of cases) {
+            expect(toLeftTransformed(input)).toStrictEqual(expected)
         }
     })
 }
